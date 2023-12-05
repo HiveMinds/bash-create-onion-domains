a7e2c1febc60743714b91d342d6d4c1d75495769
a-t-0 <you@example.com>
2023-12-05 21:16:38 +0100

    Renamed dependency functions.

--- src/main.sh
+++ src/main.sh
@@ -25,13 +25,13 @@ load_dependency_manager
 
 # Load required dependencies.
 for required_dependency in "${CREATE_ONION_DOMAINS_REQUIRED_DEPS[@]}"; do
-  load_required_dependency "$START_TOR_AT_BOOT_PATH" "$required_dependency"
+  load_required_dependency "$CREATE_ONION_DOMAINS_PATH" "$required_dependency"
 done
 
 # Load dependencies that can be a parent dependency (=this module is a
 # dependency of that module/dependency).
 for parent_dep in "${CREATE_ONION_DOMAINS_PARENT_DEPS[@]}"; do
-  load_parent_dependency "calling_repo_root_path" "$START_TOR_AT_BOOT_PATH" "$parent_dep"
+  load_parent_dependency "$CREATE_ONION_DOMAINS_PATH" "$parent_dep"
 done
 
 LOG_LEVEL_ALL # set log level to all, otherwise, NOTICE, INFO, DEBUG, TRACE will not be logged.
