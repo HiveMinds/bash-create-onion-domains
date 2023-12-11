68e5651ea6ad7c043a3eb155cf9abc7ced5ea62d
a-t-0 <you@example.com>
2023-12-11 16:49:27 +0100

    Prevend CLI args from being passed when loading dependency main.

--- src/json_to_torrc.sh
+++ src/json_to_torrc.sh
@@ -59,7 +59,7 @@ function write_json_to_torrc() {
   # Set the socksport.
   #echo "SocksPort $SOCKS_PORT" | sudo tee "$torrc_output_filepath" >/dev/null
   # echo "$torrc_content" | sudo tee -a "$torrc_output_filepath" >/dev/null # The a is for append.
-  echo "$torrc_content" | sudo tee -a "$torrc_output_filepath" >/dev/null
+  echo "$torrc_content" | sudo tee "$torrc_output_filepath" >/dev/null
 
   manual_assert_file_exists "$torrc_output_filepath"
   NOTICE "Wrote torrc_content to $torrc_output_filepath"
